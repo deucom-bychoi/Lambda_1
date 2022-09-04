e4f6b67194eea21fe60e15d8ea406e19bf8493fa
deucom-bychoi <apexarm@naver.com>
2022-09-04 17:30:48 +0900

    program.cs 파일 수정

--- Lambda_1/Program.cs
+++ Lambda_1/Program.cs
@@ -11,6 +11,8 @@ namespace Lambda_1
         static void Main(string[] args)
         {
             Console.WriteLine("Git Test");
+            Console.WriteLine("File update 2");
+
         }
     }
 }
